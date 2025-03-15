b1b263ae1e84b0721e4788b96c0d26c52677eb62
Choi Yong Won <cywin0821@gmail.com>
2025-03-15 21:53:56 +0900

    chore : develop branch 생성 (개발용, main에 배포)

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "@pages/HomePage.tsx";
+import HomePage from "@pages/home/HomePage.tsx";
 
 const AppRouter = () => {
   return (
