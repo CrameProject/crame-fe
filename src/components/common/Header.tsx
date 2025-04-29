98335a7fd9964e8ece7de77966046a1ca426d4f5
Choi Yong Won <cywin0821@gmail.com>
2025-04-29 21:11:20 +0900

    feat : [#1] 반응형 헤더 컴포넌트 초안 제작

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,13 @@
+import LogoIcon from "@/assets/icons/Logo.svg?react";
+
 const Header = () => {
   return (
-    <header className="w-full min-w-[768px] bg-amber-200 py-3">
+    <header className="w-full min-w-[768px] py-3">
       <div className="mx-auto flex max-w-screen-xl items-center justify-between px-12">
         <div className="flex items-center gap-8">
-          <div className="text-xl font-bold text-blue-600">MyLogo</div>
+          <div className="text-xl font-bold text-blue-600">
+            <LogoIcon className="h-8" />
+          </div>
           <nav className="flex items-center gap-6 px-16 font-medium text-gray-700">
             <a href="#" className="transition hover:text-blue-500">
               차트
@@ -21,7 +25,7 @@ const Header = () => {
         </div>
 
         <div className="flex items-center gap-4">
-          <button className="rounded border border-blue-500 px-6 py-2 text-blue-500 transition hover:bg-blue-50">
+          <button className="rounded border px-6 py-2 text-blue-500 transition hover:bg-blue-50">
             로그인
           </button>
           <button className="rounded bg-blue-500 px-6 py-2 text-white transition hover:bg-blue-600">
@@ -32,4 +36,5 @@ const Header = () => {
     </header>
   );
 };
+
 export default Header;
