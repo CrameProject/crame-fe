f80d6c0631f9869a5cef08cf2e3363e2a2b8f8f7
Choi Yong Won <cywin0821@gmail.com>
2025-04-29 21:19:32 +0900

    feat : [#1] 반응형 헤더 컴포넌트의 버튼, 커스텀 버튼으로 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import LogoIcon from "@/assets/icons/Logo.svg?react";
 
 const Header = () => {
@@ -8,6 +9,7 @@ const Header = () => {
           <div className="text-xl font-bold text-blue-600">
             <LogoIcon className="h-8" />
           </div>
+
           <nav className="flex items-center gap-6 px-16 font-medium text-gray-700">
             <a href="#" className="transition hover:text-blue-500">
               차트
@@ -25,12 +27,13 @@ const Header = () => {
         </div>
 
         <div className="flex items-center gap-4">
-          <button className="text-gray-1500 rounded border border-gray-200 px-6 py-2 transition hover:bg-gray-100">
+          <Button
+            variant="outline"
+            className="h-9 border-gray-300 px-6 text-gray-800 hover:bg-gray-100"
+          >
             회원가입
-          </button>
-          <button className="rounded bg-[#F4B224] px-6 py-2 text-white transition hover:bg-[#d9991b]">
-            로그인
-          </button>
+          </Button>
+          <Button className="h-9 bg-[#F4B224] px-6 text-white hover:bg-[#d9991b]">로그인</Button>
         </div>
       </div>
     </header>
