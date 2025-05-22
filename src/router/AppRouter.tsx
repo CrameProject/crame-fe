ccb7f722040b93f844557735573af00254335fb1
Choi Yong Won <cywin0821@gmail.com>
2025-05-22 23:05:33 +0900

    feat : [#8] 라우팅 수정

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "@pages/home/HomePage.tsx";
 import TradingPage from "@pages/trade/TradingPage.tsx";
+import InfoPage from "@pages/trade/info/InfoPage.tsx";
+import InvestmentNotice from "@pages/trade/info/investment/InvestmentNotice.tsx";
+import ApiKeyPage from "@pages/trade/info/ApiKey/ApiKeyPage.tsx";
 
 const AppRouter = () => {
   return (
@@ -8,6 +11,11 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/trade" element={<TradingPage />} />
+        <Route path="/trade/info" element={<InfoPage />}>
+          <Route index element={<Navigate to="/trade/info/notice" replace />} />
+          <Route path="notice" element={<InvestmentNotice />} />
+          <Route path="api" element={<ApiKeyPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
