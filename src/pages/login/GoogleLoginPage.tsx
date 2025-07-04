8049211ea907aca571b964a2f4fa97c1831a26a7
Choi Yong Won <cywin0821@gmail.com>
2025-07-04 09:34:00 +0900

    feat :[#22] 구글 로그인 페이지 데모 추가

---
+++ src/pages/login/GoogleLoginPage.tsx
@@ -0,0 +1,11 @@
+import GoogleButton from "./components/GoogleButton";
+
+const GoogleLoginPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <GoogleButton/>
+        </div>
+    )
+}
+
+export default GoogleLoginPage;
